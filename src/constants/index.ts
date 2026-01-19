efbdd877ee72db43bcf145d7d41680a1679013ae
Ozair Aziz <uzair.aziz34@gmail.com>
2026-01-19 19:55:21 +0000

    Add themeContext, update constants files and update main.tsx to include themeProvider wrapper

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,13 @@
 export const API_URL = import.meta.env.VITE_API_URL
 
 export const USERS_PER_PAGE = 6
+
+export type Theme = 'light' | 'dark'
+
+export interface ThemeContextType {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+  toggleTheme: () => void
+}
+
+export const THEME_STORAGE_KEY = 'themeMode'
